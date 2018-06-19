4f53f1c11a8763d1a9d049a123610d5c032ccef8
Ryan Nowak <nowakra@gmail.com>
2018-06-19 16:10:30 -0700

    project skeleton

---
+++ src/dotnet-pluggr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pluggr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
